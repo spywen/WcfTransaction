784ccd61576af2eebaaef0401b07d7958630c98e
Laurent Babin <spywen@hotmail.fr>
2016-03-25 11:39:31 +0100

    First commit

---
+++ WcfTransactionClient/WcfTransactionClient/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace WcfTransactionClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //--- Introduce console program ---
+            Console.WriteLine("--- WCF TRANSACTION ---");
+            Console.WriteLine("Press enter to start process...");
+            var name = Console.ReadLine();
+
+            //List of users to create
+            var users = new List<User> {
+                new User{Name = "Kmille"},
+                new User{Name = "Laurentt"},
+                new User{Name = "Agathe"}
+            };
+
+            //Call complex process to create user
+            CreateUsers(users);
+
+            //--- End of the program ---
+            Console.WriteLine("--- END OF WCF TRANSACTION  (PRESS ENTER) ---");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Create users (suppose complex process)
+        /// </summary>
+        /// <param name="users"></param>
+        public static void CreateUsers(List<User> users)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.RequiresNew))
+            {
+                var dbAContext = new DbAEntities();
+                try
+                {
+                    foreach (var user in users)
+                    {
+                        //DbB modification : WCF call
+                        var wcfTransactSvc = new WcfTransactionService.Service1Client();
+                        wcfTransactSvc.CreateDefaultUser();
+                        var result = wcfTransactSvc.CreateUser(user.Name + "123");
+                        if (!result)
+                        {
+                            throw new Exception("");
+                        }
+                        //DbA modification : local
+                        dbAContext.User.Add(user);
+                    }
+                    //SAVE LOCALLY
+                    dbAContext.SaveChanges();
+                    //SAVE ON WCF
+                    ts.Complete();
+                }
+                catch (Exception e)
+                {
+                    Trace.TraceError("ERROR DURING CREATION", e);
+                }
+                finally
+                {
+                    ts.Dispose();
+                    dbAContext.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create one user
+        /// </summary>
+        /// <param name="user"></param>
+        public void CreateUser(User user)
+        {
+
+        }
+    }
+}
---
+++ WcfTransactionService/WcfTransactionService/Service1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfTransactionService
+{
+    [ServiceBehavior(TransactionIsolationLevel = System.Transactions.IsolationLevel.Serializable)]
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class Service1 : IService1
+    {
+        [OperationBehavior(TransactionScopeRequired = true, TransactionAutoComplete = true)]
+        public bool CreateUser(string name)
+        {
+            try
+            {
+                var newUser = new User();
+                newUser.Name = name;
+
+                var test = new DbBEntities();
+                test.User.Add(newUser);
+
+                test.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Error", e);
+                return false;
+            }
+        }
+
+        public bool CreateDefaultUser()
+        {
+            try
+            {
+                var newUser = new User();
+                newUser.Name = "Default";
+
+                var test = new DbBEntities();
+                test.User.Add(newUser);
+
+                test.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Error", e);
+                return false;
+            }
+        }
+    }
+}
